0fb65a1799d268d8cfe1362efaf37b45652d606b
jiyou384 <danny384@naver.com>
2021-10-05 17:29:22 +0900

    calculate centroid of roi and visualize in rviz

--- lidar/include/lidar/vlp16_subscriber.h
+++ lidar/include/lidar/vlp16_subscriber.h
@@ -2,9 +2,9 @@
 #include <iostream>
 #include <vector>
 #include <sensor_msgs/PointCloud2.h>
-#include <sensor_msgs/PointCloud.h>
 #include <sensor_msgs/point_cloud_conversion.h>
 #include <geometry_msgs/Point.h>
+#include <visualization_msgs/Marker.h>
 
 // PCL specific includes
 #include <pcl_conversions/pcl_conversions.h>
@@ -33,13 +33,14 @@ public:
     void publish(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud2pub);
     void setROI(pcl::PointCloud<pcl::PointXYZ>::Ptr lidar_cloud_ptr);
     double theta(const double &x, const double &y);
+    void centroid(int size, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered);
+    void visualize(float x,float y, float z);
 
-    pcl::PointCloud<pcl::PointXYZ> lidar_cloud;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr lidar_cloud_ptr;
     pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud;
 
     ros::Subscriber sub;
     ros::Publisher pub;
+    ros::Publisher pub2;
     ros::NodeHandle nh;
 };
 }
