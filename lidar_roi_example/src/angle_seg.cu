a00ab04c9f5b96b76736b47afb79646403cca765
jiyou384 <danny384@g.skku.edu>
2022-09-13 12:32:41 +0900

    use thrust vector

--- lidar_roi_example/src/angle_seg.cu
+++ lidar_roi_example/src/angle_seg.cu
@@ -1,6 +1,10 @@
 #include "lidar_roi_example/angle_seg.h"
+
 #include "assert.h"
 #include "ros/ros.h"
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
 
 namespace ROIExample
 {
@@ -14,14 +18,14 @@ namespace ROIExample
         return result;
     }
 
-    __global__ void AzimuthSeg(pcl::PointCloud<pcl::PointXYZ> *&d_pc_in, pcl::PointCloud<pcl::PointXYZ> *&d_pc_prj)
+    __global__ void AzimuthSeg(pcl::PointXYZ::Ptr d_pc_in, pcl::PointXYZ::Ptr d_pc_prj)
     {
         int idx = blockIdx.x * blockDim.x + threadIdx.x;
         if (!(idx % 3))
         {
-            float &x = d_pc_in->at(idx).x;
-            float &y = d_pc_in->at(idx).y;
-            float &z = d_pc_in->at(idx).z;
+            float &x = (*(d_pc_in)[idx]).x;
+            float &y = (*(d_pc_in)[idx]).y;
+            float &z = (*(d_pc_in)[idx]).z;
 
             float theta = atan2(x, -y) * 180 / M_PI - 90;
             theta = (theta < 0) ? theta + 360 : theta;
@@ -32,32 +36,36 @@ namespace ROIExample
                 point.x = x;
                 point.y = y;
                 point.z = z;
-                d_pc_prj->points.push_back(point);
+                d_pc_prj->push_back(point);
             }
         }
     }
+    
     void AngleSeg::Init(pcl::PointCloud<pcl::PointXYZ> *&input)
     {
         auto start = ros::Time::now();
 
         // allocate memory for device
         int size = input->points.size();
-        float *d_pc_in;
-        int *d_pc_prj;
+        thrust::device_vector<pcl::PointXYZ> d_pc_in;
+        thrust::device_vector<pcl::PointXYZ> d_pc_prj;
+
         pcl::PointCloud<pcl::PointXYZ>* pc_seg;
 
-        checkCuda(cudaMallocManaged(&d_pc_in, sizeof(pcl::PointXYZ) * size));
-        checkCuda(cudaMallocManaged(&d_pc_prj, sizeof(pcl::PointXYZ) * size));
+        // checkCuda(cudaMallocManaged(&d_pc_in, sizeof(pcl::PointXYZ) * size));
+        // checkCuda(cudaMallocManaged(&d_pc_prj, sizeof(pcl::PointXYZ) * size));
 
         // copy pointcloud data to device
-        checkCuda(cudaMemcpy(d_pc_in, input, sizeof(pcl::PointXYZ) * size, cudaMemcpyHostToDevice));
+        checkCuda(cudaMemcpy(d_pc_in, input->points, sizeof(pcl::PointXYZ) * size, cudaMemcpyHostToDevice));
+        pcl::PointXYZ::Ptr d_pc_ptr = thrust::raw_pointer_cast(d_pc_in.data);
+        pcl::PointXYZ::Ptr d_pc_prj_ptr = thrust::raw_pointer_cast(d_pc_prj.data);
 
         // 170 blocks, 1020 threads
         // 1020/3 = 340 points per block
         // 170 x 340 = 57800 maximum points
         AzimuthSeg<<<170, 1020>>>(d_pc_in, d_pc_prj);
 
-        checkCuda(cudaMemcpy(pc_seg, d_pc_prj, sizeof(pcl::PointXYZ) * size, cudaMemcpyHostToDevice));
+        checkCuda(cudaMemcpy(pc_seg->points, d_pc_prj, sizeof(pcl::PointXYZ) * size, cudaMemcpyHostToDevice));
         pc_seg_ = pc_seg;
 
         auto end = ros::Time::now();
